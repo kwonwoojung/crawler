511d1fadb000b599beb8e44c9a282ef018115a8e
kwonwoojung <kwonwj123@gmail.com>
2017-05-16 08:09:07 +0000

    hiw

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,11 +4,9 @@ class Article < ApplicationRecord
     [1,11,21,31].each do |index|
       page = agent.get"https://search.naver.com/search.naver?sm=tab_hty.top&where=news&ie=utf8&query=#{helper}&start=#{index}"
       list = page.search("a._sp_each_title").map(&:text)
-      
       list.each do |title|
-        article_title = Article.create(title: "#{title}")
+       Article.create(:title => title)
       end
     end
   end
 end
-
